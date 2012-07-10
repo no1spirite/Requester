b531251c97548b8e8eaead2aeff969ba109aa6b2
Ryan Robinson <ryan.robinson@red-badger.com>
2012-07-10 16:51:00 +0100

    Clean up

--- src/Requester/Program.cs
+++ src/Requester/Program.cs
@@ -1,118 +1,117 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net;
-using System.Net.Http;
-using System.Text;
-using System.Threading;
-using Topshelf;
-using log4net.Config;
-
-namespace Requester
+﻿namespace Requester
 {
-	class Program
-	{
-		private const string _urlFile = "url.txt";
-		private const string _intervalFile = "interval.txt";
-		private CancellationToken _token;
-		private CancellationTokenSource _tokenSource;
-		private TimeSpan _timeout;
+    using System;
+    using System.IO;
+    using System.Net.Http;
+    using System.Threading;
 
-		static void Main(string[] args)
-		{
-			BasicConfigurator.Configure();
+    using Topshelf;
 
-			if (args.Length < 2)
-			{
-				PrintHelp();
-				Environment.Exit(-1);
-				return;
-			}
+    using log4net.Config;
 
-			var isInstallOrUninstall = args[0].Contains("install");
-			var url = args[isInstallOrUninstall ? 1 : 0];
-			var interval = args[isInstallOrUninstall? 2 : 1];
+    internal class Program
+    {
+        private const string IntervalFile = "interval.txt";
 
-			int res;
-			if (!int.TryParse(interval, out res))
-			{
-				PrintHelp();
-				Environment.Exit(-1);
-				return;
-			}
+        private const string UrlFile = "url.txt";
 
-			if (!File.Exists(_urlFile))
-			{
-				File.WriteAllText(_urlFile, "http://google.com");
-			}
+        private CancellationToken token;
 
-			if (!File.Exists(_intervalFile))
-			{
-				File.WriteAllText(_intervalFile, "40");
-			}
+        private CancellationTokenSource tokenSource;
 
-			Thread.CurrentThread.Name = "Requester Entrypoint Thread";
+        private static void Main(string[] args)
+        {
+            BasicConfigurator.Configure();
 
-			HostFactory.Run(x =>
-				{
-					x.Service<Program>(s =>
-						{
-							s.ConstructUsing(name => new Program());
-							s.WhenStarted(p => p.Start(File.ReadAllText(_urlFile), int.Parse(File.ReadAllText(_intervalFile))));
-							s.WhenStopped(p => p.Stop());
-						});
-					x.RunAsLocalSystem();
-					x.BeforeInstall(() =>
-						{
-							File.WriteAllText(_urlFile, args[1]);
-							File.WriteAllText("inteval.txt", interval);
-						});
-					x.SetDescription(string.Format("Requests an url every interval"));
-					x.SetDisplayName("Requester");
-					x.SetServiceName("Requester");
-				});
-		}
+            if (args.Length < 2)
+            {
+                PrintHelp();
+                Environment.Exit(-1);
+            }
 
-		private static void PrintHelp()
-		{
-			Console.WriteLine(
-@"Too few args: requester install <url> <interval>
+            bool isInstallOrUninstall = args[0].Contains("install");
+            string interval = args[isInstallOrUninstall ? 2 : 1];
 
-Sample usage; request
- url: ""https://github.com""
- interval: 300 seconds
+            int res;
+            if (!int.TryParse(interval, out res))
+            {
+                PrintHelp();
+                Environment.Exit(-1);
+            }
 
-requester install https://github.com 300
-");
-		}
+            if (!File.Exists(UrlFile))
+            {
+                File.WriteAllText(UrlFile, "http://portfolio-14.apphb.com/");
+            }
 
-		private void Start(string url, int interval)
-		{
-			_tokenSource = new CancellationTokenSource();
-			_token = _tokenSource.Token;
+            if (!File.Exists(IntervalFile))
+            {
+                File.WriteAllText(IntervalFile, "40");
+            }
 
-			while (!_token.IsCancellationRequested)
-			{
-				//httpClient.DefaultRequestHeaders
-				//    .Add("Authorization", string.Format("Basic {0}",
-				//        Convert.ToBase64String(Encoding.UTF8.GetBytes("{0}:{1}".FormatWith(_username, _password)))));
+            Thread.CurrentThread.Name = "Requester Entrypoint Thread";
 
-				try
-				{
-					new HttpClient().GetAsync(url).Wait(20000, _token);
-				}
-				catch (Exception e)
-				{
-					Console.WriteLine(e);
-				}
+            HostFactory.Run(
+                x =>
+                    {
+                        x.Service<Program>(
+                            s =>
+                                {
+                                    s.ConstructUsing(name => new Program());
+                                    s.WhenStarted(
+                                        p =>
+                                        p.Start(File.ReadAllText(UrlFile), int.Parse(File.ReadAllText(IntervalFile))));
+                                    s.WhenStopped(p => p.Stop());
+                                });
+                        x.RunAsLocalSystem();
+                        x.BeforeInstall(
+                            () =>
+                                {
+                                    File.WriteAllText(UrlFile, args[1]);
+                                    File.WriteAllText("inteval.txt", interval);
+                                });
+                        x.SetDescription(string.Format("Requests an url every interval"));
+                        x.SetDisplayName("Requester");
+                        x.SetServiceName("Requester");
+                    });
+        }
 
-				Thread.Sleep(TimeSpan.FromSeconds(interval));
-			}
-		}
+        private static void PrintHelp()
+        {
+            Console.WriteLine(
+                @"Too few args: requester install <url> <interval>
 
-		private void Stop()
-		{
-			_tokenSource.Cancel();
-		}
-	}
+                Sample usage; request
+                 url: ""https://github.com""
+                 interval: 300 seconds
+
+                requester install https://github.com 300
+                ");
+        }
+
+        private void Start(string url, int interval)
+        {
+            this.tokenSource = new CancellationTokenSource();
+            this.token = this.tokenSource.Token;
+
+            while (!this.token.IsCancellationRequested)
+            {
+                try
+                {
+                    new HttpClient().GetAsync(url).Wait(20000, this.token);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+
+                Thread.Sleep(TimeSpan.FromSeconds(interval));
+            }
+        }
+
+        private void Stop()
+        {
+            this.tokenSource.Cancel();
+        }
+    }
 }
